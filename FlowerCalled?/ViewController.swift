6404b849f83fda4110fa674b173c7c845dcce7b5
AmirReza Jamali <amir7jamali.ar@yahoo.com>
2023-02-28 18:15:35 +0330

    core ml model added to detect photo

--- FlowerCalled?/ViewController.swift
+++ FlowerCalled?/ViewController.swift
@@ -20,6 +20,27 @@ class ViewController: UIViewController {
         imagePicker.sourceType = .camera
         imagePicker.allowsEditing = false
     }
+    func detectFlower (_ photo: CIImage) {
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier(configuration: MLModelConfiguration()).model) else {
+            return
+        }
+        
+        let request = VNCoreMLRequest(model: model) { requset, error in
+            guard let result = requset.results as? [VNClassificationObservation] else {
+                return
+            }
+            print(result)
+            if let firstResult = result.first {
+                self.navigationItem.title = firstResult.identifier
+            }
+        }
+        let handler = VNImageRequestHandler(ciImage: photo)
+        do {
+            try handler.perform([request])
+        } catch {
+            print("Error getting result from VN model")
+        }
+    }
     func openPhotoLibraryPicker () {
         var pickerConfig = PHPickerConfiguration(photoLibrary: .shared())
         pickerConfig.selectionLimit = 1
@@ -36,6 +57,7 @@ class ViewController: UIViewController {
         present(imagePicker, animated: true)
     }
 }
+
 extension ViewController: PHPickerViewControllerDelegate {
     func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
         picker.dismiss(animated: true)
@@ -44,19 +66,24 @@ extension ViewController: PHPickerViewControllerDelegate {
                 guard let selectedImage = reading as? UIImage else {
                     return
                 }
+                if let ciImage = CIImage(image: selectedImage) {
+                    self.detectFlower(ciImage)
+                }
                 DispatchQueue.main.async {
                     self.mainImageView.image = selectedImage
                 }
             }
         }
     }
-    
-    
 }
+
 extension ViewController: UIImagePickerControllerDelegate {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         guard let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
             fatalError("Error loading photo from camera.")
+        }
+        if let ciImage = CIImage(image: userPickedImage) {
+            detectFlower(ciImage)
         }
         DispatchQueue.main.async {
             self.mainImageView.image = userPickedImage
